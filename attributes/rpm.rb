38903e20ed865e1cd9c2b34eb35ca53ed52a041c
Dan Maglasang <dcmaglasang@gmail.com>
2014-04-19 01:42:15 -0400

    Update rpm.rb

--- attributes/rpm.rb
+++ attributes/rpm.rb
@@ -1,2 +1,2 @@
-default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.0.noarch.rpm"
-default.elasticsearch[:rpm_sha] = "05eb5649a4b772ee437acc47a6834cd6dd26f245"
+default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.noarch.rpm"
+default.elasticsearch[:rpm_sha] = "9f441bb3bf945715b05c378867cda513a23e111b"
